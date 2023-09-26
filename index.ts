8d06395d5f335e0c2d6460a0900b1a73fa92b683
AndersonPadovani <andersonpadovani@protonmail.com>
2023-09-26 20:19:57 -0300

    Add section tecnologics

--- index.ts
+++ index.ts
@@ -1,1 +1,36 @@
-console.log("Compilado!");
+function startPage() {
+  selectTecIcon("python");
+}
+
+const tecIcons = document.querySelector(".itensTecIcon");
+tecIcons?.addEventListener("click", (event) => {
+  const element = event.target as HTMLInputElement;
+  if (element.id != "") {
+    selectTecIcon(element.id);
+  }
+});
+
+function selectTecIcon(id: string) {
+  const listTecInfo = {
+    github: "ghInfo",
+    java: "javaInfo",
+    js: "jsInfo",
+    python: "pyInfo",
+    react: "reactInfo",
+    sql: "sqlInfo",
+    nodejs: "nodejsInfo",
+  };
+
+  Object.entries(listTecInfo).forEach(([key, value]) => {
+    let element = document.getElementById(key);
+    let info = document.getElementById(value) as HTMLObjectElement;
+    element?.classList.remove("infoActive");
+
+    info.style.display = "none";
+
+    if (element?.id === id) {
+      info.style.display = "block";
+      element.classList.add("infoActive");
+    }
+  });
+}
--- src/dist/index.js
+++ src/dist/index.js
@@ -1,2 +1,32 @@
 "use strict";
-console.log("Compilado!");
+function startPage() {
+    selectTecIcon("python");
+}
+const tecIcons = document.querySelector(".itensTecIcon");
+tecIcons?.addEventListener("click", (event) => {
+    const element = event.target;
+    if (element.id != "") {
+        selectTecIcon(element.id);
+    }
+});
+function selectTecIcon(id) {
+    const listTecInfo = {
+        github: "ghInfo",
+        java: "javaInfo",
+        js: "jsInfo",
+        python: "pyInfo",
+        react: "reactInfo",
+        sql: "sqlInfo",
+        nodejs: "nodejsInfo",
+    };
+    Object.entries(listTecInfo).forEach(([key, value]) => {
+        let element = document.getElementById(key);
+        let info = document.getElementById(value);
+        element?.classList.remove("infoActive");
+        info.style.display = "none";
+        if (element?.id === id) {
+            info.style.display = "block";
+            element.classList.add("infoActive");
+        }
+    });
+}
