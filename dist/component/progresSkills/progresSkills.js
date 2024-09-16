db6efdc00576015002430f84758bd85184307001
AndersonPadovani <andersonpadovani@protonmail.com>
2024-09-16 20:16:03 -0300

    Create tooltip to skillsLevel

--- dist/component/progresSkills/progresSkills.js
+++ dist/component/progresSkills/progresSkills.js
@@ -12,10 +12,24 @@ const ProgressSkill = ({ progress, title }) => {
     progressBar.className = "progressBar";
     progressBar.max = 100;
     progressBar.value = progress;
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("tooltipSkill");
+    tooltip.style.opacity = "0";
+    tooltip.innerHTML = `Dominio de ${progress}%`;
+    //Função tooltip on mouse enter skill
+    divProgress.addEventListener("mouseenter", (element) => {
+        //Create delay to show tooltip
+        setTimeout(() => {
+            tooltip.style.opacity = "100";
+        }, 1000);
+    });
+    //Função tooltip on mouse leave skill
+    divProgress.addEventListener("mouseleave", (element) => (tooltip.style.opacity = "0"));
     divContent.appendChild(spanTitle);
     divContent.appendChild(progressBar);
     divProgress.appendChild(imageSkillIcon);
     divProgress.appendChild(divContent);
+    divProgress.appendChild(tooltip);
     skills?.appendChild(divProgress);
 };
 export default ProgressSkill;
--- src/component/progresSkills/progresSkills.ts
+++ src/component/progresSkills/progresSkills.ts
@@ -23,11 +23,31 @@ const ProgressSkill = ({ progress, title }: ProgressSkillType) => {
   progressBar.max = 100;
   progressBar.value = progress;
 
+  const tooltip = document.createElement("div");
+  tooltip.classList.add("tooltipSkill");
+  tooltip.style.opacity = "0";
+  tooltip.innerHTML = `Dominio de ${progress}%`;
+
+  //Função tooltip on mouse enter skill
+  divProgress.addEventListener("mouseenter", (element) => {
+    //Create delay to show tooltip
+    setTimeout(() => {
+      tooltip.style.opacity = "100";
+    }, 1000);
+  });
+
+  //Função tooltip on mouse leave skill
+  divProgress.addEventListener(
+    "mouseleave",
+    (element) => (tooltip.style.opacity = "0")
+  );
+
   divContent.appendChild(spanTitle);
   divContent.appendChild(progressBar);
 
   divProgress.appendChild(imageSkillIcon);
   divProgress.appendChild(divContent);
+  divProgress.appendChild(tooltip);
   skills?.appendChild(divProgress);
 };
 
